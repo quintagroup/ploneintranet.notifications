f8ec77416b03037f7934194f5ca6c59bb693162c
Alexander Pilz <pilz@syslab.com>
2014-11-19 13:44:48 +0100

    new

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='ploneintranet.notifications',
+      version=version,
+      description="A package to provide a notifications backend",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone intranet notifications',
+      author='Plone Intranet Consortium',
+      author_email='ploneintranet@netsight.co.uk',
+      url='https://github.com/ploneintranet/ploneintranet.notifications',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['ploneintranet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
