c5f7a70ff2a9a206c84a0a99fb0eb47aff90115d
Alessandro Pisa <alessandro.pisa@redturtle.it>
2014-11-19 14:28:52 +0100

    add a local buildout

--- setup.py
+++ setup.py
@@ -21,15 +21,15 @@ setup(name='ploneintranet.notifications',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        ],
+          "Programming Language :: Python",
+      ],
       keywords='plone intranet notifications',
       author='Plone Intranet Consortium',
       author_email='ploneintranet@netsight.co.uk',
       url='https://github.com/ploneintranet/ploneintranet.notifications',
       license='gpl',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['ploneintranet'],
       include_package_data=True,
       zip_safe=False,
@@ -37,6 +37,13 @@ setup(name='ploneintranet.notifications',
           'setuptools',
           # -*- Extra requirements: -*-
       ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'plone.browserlayer',
+              'unittest2',
+          ],
+      },
       entry_points="""
       # -*- Entry points: -*-
       """,
